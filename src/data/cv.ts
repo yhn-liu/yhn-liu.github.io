a0d37ed34a199ab52c71636953bed44846ae6f75
Helios 超酷 <yuhanheliosliu@outlook.com>
2025-03-05 16:18:52 +0800

    add a publication

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -45,12 +45,12 @@ export const skills = [
 
 export const publications = [
 	{
-		title: '',
-		authors: '',
-		journal: '',
-		time: '',
-		link: '',
-		abstract: '',
+		title: 'Unveiling bias in artificial intelligence: Exploring causes and strategies for mitigation',
+		authors: 'Yuhan Liu',
+		journal: 'Applied and Computational Engineering',
+		time: 'July 2024',
+		link: 'https://www.researchgate.net/publication/382296885_Unveiling_bias_in_artificial_intelligence_Exploring_causes_and_strategies_for_mitigation',
+		abstract: "With the rapid advancement of Artificial Intelligence (AI), the emergence of various AI models such as Stable Diffusion, ChatGPT, and MidJourney has brought numerous benefits and opportunities. Through users' extensive utilization, they have discovered biases towards gender, race, and other factors in these AI systems. This paper focuses on bias in AI and aims to investigate its causes and propose strategies for mitigation. Through a comprehensive literature review, the paper has explored the phenomenon of bias in AI-generated content. Furthermore, we examine the reasons behind bias and solutions from social and intelligence science perspectives. From a social science perspective, we examine the effects of gender bias in AI and highlight the importance of incorporating diversity and gender theory in machine learning. From an intelligence science standpoint, we explore factors like biased datasets, algorithmic fairness, and the role of machine learning randomness in group fairness. Additionally, we discuss the research methodology employed, including the literature search strategy and quantity assessment. The results and discussions confirm the existence of bias in current AI products, particularly in the underrepresentation of women in the AI development field. Finally, we present future perspectives on reducing bias in AI products, including the importance of fair datasets, improved training processes, and increased participation of female engineers and intelligence scientists in the AI field. By addressing bias in AI, the paper can strive for more equitable and responsible AI systems that benefit diverse users and promote social progress.",
 	},
 	// {
 	// 	title: 'The Radiation of Uranium Compounds',
